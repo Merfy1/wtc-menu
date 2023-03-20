ae2cfe0fd7dc88ca8f778aec6364a024bc0233f9
ArmAtrix123 <alexstor2003@gmail.com>
2023-03-20 03:42:36 +0300

    create: slider

--- src/components/MenuSlider/MenuSlider.jsx
+++ src/components/MenuSlider/MenuSlider.jsx
@@ -1,28 +1,21 @@
 import React, { useState } from "react";
 import './menuSlider.css'
 import { FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+import Slider from "../Slider/Slider";
 
 export function MenuSlider() {
+    const images = [
+        "img/slider1.png",
+        "img/slider2.png",
+        "img/slider3.png",
+        "img/slider4.png",
+        "img/slider5.png",
+        "img/slider6.png",
+      ];
     return (
         <div className="slider">
             <div className="slider-container">
-                <div className="slider-wrapper">
-                        <button className="button-nav">
-                            <FiArrowLeftCircle className="arrow-left"></FiArrowLeftCircle>
-                        </button>
-                        <img src="img/slider 1.png" alt="" className="slider-image"/>
-                        <button className="button-nav">
-                            <FiArrowRightCircle  className="arrow-right"></FiArrowRightCircle>
-                        </button>
-                </div>
-                <div className="slider-wrapper nav">
-                    <img src="img/slider-nav-active.svg" alt="" className="slider-nav"/>
-                    <img src="img/slider-nav.svg" alt="" className="slider-nav"/>
-                    <img src="img/slider-nav.svg" alt="" className="slider-nav"/>
-                    <img src="img/slider-nav.svg" alt="" className="slider-nav"/>
-                    <img src="img/slider-nav.svg" alt="" className="slider-nav"/>
-                    <img src="img/slider-nav.svg" alt="" className="slider-nav"/>
-                </div>
+                <Slider images={images} />
             </div>
         </div>
     )
---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect  } from "react";
+import { FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+
+const Slider = ({ images }) => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    // автоматическое перелистывание
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setCurrentSlide((currentSlide) =>
+          currentSlide === images.length - 1 ? 0 : currentSlide + 1
+        );
+      }, 3000);
+      return () => clearInterval(interval);
+    }, [images]);
+  
+    // переход на следующий слайд
+    const nextSlide = () => {
+      setCurrentSlide((currentSlide) =>
+        currentSlide === images.length - 1 ? 0 : currentSlide + 1
+      );
+    };
+  
+    // переход на предыдущий слайд
+    const prevSlide = () => {
+      setCurrentSlide((currentSlide) =>
+        currentSlide === 0 ? images.length - 1 : currentSlide - 1
+      );
+    };
+  
+    // переход на выбранный слайд
+    const goToSlide = (index) => {
+      setCurrentSlide(index);
+    };
+  
+    return (
+      <div className="slider">
+        <div className="slides">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className={`slide ${currentSlide === index ? "active" : ""}`}
+              style={{ backgroundImage: `url(${image})` }}
+            ></div>
+          ))}
+        </div>
+        <div className="buttons">
+          <button onClick={prevSlide} className='button-left'>
+            <FiArrowLeftCircle  className="arrow-left"></FiArrowLeftCircle>
+          </button>
+          <button onClick={nextSlide} className='button-right'>
+            <FiArrowRightCircle  className="arrow-right"></FiArrowRightCircle>
+          </button>
+        </div>
+        <div className="dots">
+          {images.map((image, index) => (
+            <span
+              key={index}
+              className={`dot ${currentSlide === index ? "active" : ""}`}
+              onClick={() => goToSlide(index)}
+            ></span>
+          ))}
+        </div>
+      </div>
+    );
+}
+
+export default Slider;
