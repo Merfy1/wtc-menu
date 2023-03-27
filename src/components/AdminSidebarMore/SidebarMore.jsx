adcca4d2e159231fa7788f3ac2698a264e5bde0c
ArmAtrix123 <alexstor2003@gmail.com>
2023-03-27 03:23:06 +0300

    create: sidebarmore function

--- src/components/AdminSidebarMore/SidebarMore.jsx
+++ src/components/AdminSidebarMore/SidebarMore.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./sidebarMore.css"
 import { BsDot } from 'react-icons/bs';
 
 export function SidebarMore() {
+    const [selectedPosition, setSelectedPosition] = useState(null);
+    const handlePositionClick = (position) => {
+        if (selectedPosition === position) {
+          setSelectedPosition(null);
+        } else {
+          setSelectedPosition(position);
+        }
+    };
     return (
         <>
             <div className="sidebarmore">
@@ -11,20 +19,20 @@ export function SidebarMore() {
                         <h1 className="sidebarmore-title">
                             Подробнее
                         </h1>
-                        <div className='line'/>
+                        <div className='sidebarmore-line'/>
                         <div className="sidebarmore-items">
-                            <div className="sidebarmore-item">
-                                <BsDot></BsDot>
-                                <span>Вывод</span>
-                            </div>
-                            <div className="sidebarmore-item">
-                                <BsDot></BsDot>
-                                <span>Добавить</span>
-                            </div>
-                            <div className="sidebarmore-item">
-                                <BsDot></BsDot>
-                                <span>Изменить</span>
-                            </div>
+                            <button className={selectedPosition === 1? 'sidebarmore-item_active' : 'sidebarmore-item'} onClick={() => handlePositionClick(1)}>
+                                <BsDot className='sidebarmore-icon'></BsDot>
+                                <span className='sidebarmore-text'>Вывод</span>
+                            </button>
+                            <button className={selectedPosition === 2? 'sidebarmore-item_active' : 'sidebarmore-item'} onClick={() => handlePositionClick(2)}>
+                                <BsDot className='sidebarmore-icon'></BsDot>
+                                <span className='sidebarmore-text'>Добавить</span>
+                            </button>
+                            <button className={selectedPosition === 3? 'sidebarmore-item_active' : 'sidebarmore-item'} onClick={() => handlePositionClick(3)}>
+                                <BsDot className='sidebarmore-icon'></BsDot>
+                                <span className='sidebarmore-text'>Изменить</span>
+                            </button>
                         </div>
                     </div>
                 </div>
