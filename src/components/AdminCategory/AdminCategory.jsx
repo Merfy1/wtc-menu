6b0bc30be9a834aa8ee8a2bfbb6a94ca081f5550
ArmAtrix123 <alexstor2003@gmail.com>
2023-04-01 20:31:59 +0300

    create: post category admin panel

--- src/components/AdminCategory/AdminCategory.jsx
+++ src/components/AdminCategory/AdminCategory.jsx
@@ -3,10 +3,16 @@ import axios from 'axios';
 import { BsPencil,  BsTrash } from 'react-icons/bs';
 import "./admincategory.css"
 import { AdminCategoryComponent } from '../AdminCategoryComponent/AdminCategoryComponent';
+import { AdminCreateCategory } from '../AdminCreateCategory/AdminCreateCategory';
 
 
 export function AdminCategory (){
     const [catigories, setCategories] = useState([]);
+    const [ShowAdminCreate, setShowAdminCreate] = useState(false);
+
+    const handleClick = () => {
+        setShowAdminCreate(true);
+    };
     useEffect(() => {
         axios.get('http://localhost:3001/api/admin/tags/')
         .then(res => {
@@ -26,37 +32,42 @@ export function AdminCategory (){
     }
     return (
         <>
-            <div className="main">
-                <div className="main-container">
-                    <div className="main-wrapper">
-                        <div className="main-header">
-                            <span className="main-header__title">
-                                Категория
-                            </span>
-                            <button className="main-header__button">
-                                <img src="img/plus-mini.svg" alt=""/>
-                                Добавить
-                            </button>
-                        </div>
-                        <div className="table-wrapper">
-                            <table className='main-table'>
-                                <thead>
-                                    <tr>
-                                        <th>Наименование</th>
-                                        <th className="main-table__button">Действия</th>
-                                    </tr>
-                                </thead>
-                                {catigories?.map((category) =>
-                                    <AdminCategoryComponent             
-                                        key={category.id_categoria}
-                                        category={category}
-                                        onDelete={handleDeleteCategory} />
-                                    )}
-                            </table>
+            {ShowAdminCreate ? (
+                <AdminCreateCategory/>
+            ) : (
+                <div className="main">
+                    <div className="main-container">
+                        <div className="main-wrapper">
+                            <div className="main-header">
+                                <span className="main-header__title">
+                                    Категория
+                                </span>
+                                <button className="main-header__button" onClick={handleClick}>
+                                    <img src="img/plus-mini.svg" alt=""/>
+                                    Добавить
+                                </button>
+                            </div>
+                            <div className="table-wrapper">
+                                <table className='main-table'>
+                                    <thead>
+                                        <tr>
+                                            <th>Наименование</th>
+                                            <th className="main-table__button">Действия</th>
+                                        </tr>
+                                    </thead>
+                                    {catigories?.map((category) =>
+                                        <AdminCategoryComponent             
+                                            key={category.id_categoria}
+                                            category={category}
+                                            onDelete={handleDeleteCategory} />
+                                        )}
+                                </table>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>  
+                </div>  
+            )
+            }    
         </>
     );
 };
---
+++ src/components/AdminCreateCategory/AdminCreateCategory.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { BiArrowBack, BiCheck } from 'react-icons/bi';
+import { AdminCategory } from '../AdminCategory/AdminCategory';
+export function AdminCreateCategory (){
+    const [ShowAdminCategory, setShowAdminCategory] = useState(false);
+    const [categoryName, setCategoryName] = useState('');
+    const handleCreateCategory = async () => {
+        const token = localStorage.getItem('tokenLogin'); // получаем токен из localStorage
+        try {
+          const response = await axios.post('http://localhost:3001/api/admin/tags/',
+            {
+              name: categoryName,
+              token: token,
+            }
+          );
+          console.log(response.data); // выводим ответ сервера в консоль
+        } catch (error) {
+          console.error(error); // выводим ошибку в консоль
+        }
+        setShowAdminCategory(true);
+    };
+    const handleClick = () => {
+        setShowAdminCategory(true);
+    };
+    return (
+        <>
+            {ShowAdminCategory ? (
+                <AdminCategory/>
+            ) : (
+                <div className="main">
+                    <div className="main-container">
+                        <div className="main-wrapper">
+                            <button className="main-header__back" onClick={handleClick}>
+                                <BiArrowBack/> Назад
+                            </button>
+                            <div className="main-header">
+                                <span className="main-header__title">
+                                    Создание
+                                </span>
+                                <button className="main-header__button" onClick={handleCreateCategory}>
+                                    <span><BiCheck/></span>
+                                    Сохранить
+                                </button>
+                            </div>
+                            <div className="table-wrapper">
+                                <form className='main-form'> 
+                                    <span className="main-form__span">Наименование</span>
+                                    <input value={categoryName} onChange={(e) => setCategoryName(e.target.value)} type="text" className="main-form__input"/>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+                )
+            } 
+        </>
+    );
+};
